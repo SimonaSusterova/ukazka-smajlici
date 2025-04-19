96f2d61167d98c369e34cb44bd2ca98f70b24ad3
Simona Šusterová <simsusterova@gmail.com>
2025-04-19 15:50:46 +0200

    Smajlici

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.body.innerHTML += 'funguju!'
+const selectSmiley = (evt) => {
+    evt.target.classList.add('btn-smiley--selected');
+  };
+  
+  document.querySelector('#smiley1').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley2').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley3').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley4').addEventListener('click', selectSmiley);
+  document.querySelector('#smiley5').addEventListener('click', selectSmiley);
